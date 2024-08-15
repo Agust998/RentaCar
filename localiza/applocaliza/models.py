8d959c8f05853212c0d67736956005d89567bb0f
Agustin Caraballo <agscaraballo998@gmail.com>
2024-08-15 15:41:58 -0300

    base de datos

--- localiza/applocaliza/models.py
+++ localiza/applocaliza/models.py
@@ -6,20 +6,28 @@ class Cliente(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128, null=True, blank=True)
 
+    def __str__(self):
+        return self.nombre
+
 class empleado(models.Model):
     nombre = models.CharField(max_length=40)
     apellido = models.CharField(max_length=20)
     email = models.EmailField(max_length=40)
     puesto = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.puesto
+
 class reserva(models.Model):
     fecha_de_entrega = models.DateField()
     fecha_de_devolucion = models.DateField()
-    
-    
+       
 
 class Vehiculo(models.Model):
     marca = models.CharField(max_length=30)
     modelo = models.CharField(max_length=30)
     categoria = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.categoria
 
